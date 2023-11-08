0ff6532ed796c61400cf4d4d88f21b8ccb8795ae
mehedi-hasan-2302 <104778001+mehedi-hasan-2302@users.noreply.github.com>
2023-11-08 23:59:25 +0600

    Task_101.m

--- Task_101.m
+++ Task_101.m
@@ -1,12 +1,12 @@
 P = input('Enter matrix P (in the format [a b; c d]):');
 Q = input('Enter matrix Q (in the format [a b; c d]):');
 
-maximum_value = max(P(:)); % get the maximum value of the matrix A
+maximum_value = max(P(:)); % get the maximum value of the matrix A.
 
 fprintf('Maximum value in P is:%d\n',maximum_value)
 
-if size(P,2) == size(Q,1)
-  product = P*Q;
+if size(P,2) == size(Q,1) % checking if the column length of the first matrix is equal to the row
+  product = P*Q;          % length of the second matrix.
 
   fprintf('Result:\n');
   disp(product);
